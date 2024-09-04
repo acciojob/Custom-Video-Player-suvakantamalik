2142e0500bc3afd4d36b70ae9021e31b0b68c6c1
suvakantamalik <134865642+suvakantamalik@users.noreply.github.com>
2024-09-04 13:20:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,47 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+function togglePlay(){
+	if(video.paused){
+		video.play();
+	}
+	else{
+		video.pause();
+	} 
+}
+
+function updateButton(){
+	const icons = video.paused ? "▶" : "❚❚";
+	toggle.textContent = icons;
+}
+function handleProgress() {
+	const per = (video.currentTime/video.duration)*100;
+	progressBar.style.flexBasis = `${per}%`; 
+}
+
+function skip(){
+	video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate(){
+	video[this.name] = this.value;
+}
+
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", handleProgress);
+
+skipButtons.forEach((button) => button.addEventListener("click", skip));
+
+ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach(range)
+
+toggle.addEventListener("click", () => {
+	togglePlay();
+	updateButton();
+});
+
+
+
